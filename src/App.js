eae36c61f1312ae0be1e76db79802b286c7a16e8
Kevin <tig3r86@gmail.com>
2018-06-19 08:03:03 +1000

    new code

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css"
 import { compose, graphql } from "react-apollo"
 import ListRecipes from "./queries/ListRecipes.js"
 import CreateRecipe from "./mutations/CreateRecipe.js"
+import uuidV4 from "uuid/v4"
 
 class App extends Component {
   state = {
@@ -38,21 +39,78 @@ class App extends Component {
     })
   }
 
+  addRecipe = () => {
+    const { name, ingredients, directions } = this.state
+    this.props.onAdd({
+      name,
+      ingredients,
+      directions,
+      id: uuidV4(),
+    })
+    this.setState({
+      name: "",
+      ingredient: "",
+      direction: "",
+    })
+  }
+
   render() {
+    console.log("props", this.props)
     return (
-      <div className="App">
+      <div className="App" style={style.container}>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         {this.props.recipes.map((recipe, index) => (
-          <div>
+          <div key={index}>
             <p>{recipe.name}</p>
           </div>
         ))}
+        <input
+          placeholder="Recipe name"
+          style={style.input}
+          onChange={e => this.onChange("name", e.target.value)}
+        />
+
+        <input
+          placeholder="ingredient name"
+          style={style.input}
+          onChange={e => this.onChange("name", e.target.value)}
+        />
+        <button style={style.button}>add ingredient</button>
+
+        <input
+          placeholder="direction name"
+          style={style.input}
+          onChange={e => this.onChange("name", e.target.value)}
+        />
+        <button style={style.button}>add direction</button>
+        <button style={style.button}>add recipe</button>
       </div>
     )
   }
+}
+
+const style = {
+  input: {
+    height: 50,
+    width: 450,
+    fontSize: 16,
+    border: "none",
+    borderBottom: "2px solid blue",
+    margin: 10,
+  },
+  button: {
+    height: 40,
+    width: 450,
+    margin: 10,
+  },
+  container: {
+    display: "flex",
+    flexFlow: "column nowrap",
+    alignItems: "center",
+  },
 }
 
 export default compose(
@@ -66,5 +124,13 @@ export default compose(
         recipes: props.data.listRecipes ? props.data.listRecipes.items : [],
       }
     },
+  }),
+  graphql(CreateRecipe, {
+    props: props => ({
+      onAdd: recipe =>
+        props.mutate({
+          variables: recipe,
+        }),
+    }),
   })
 )(App)
